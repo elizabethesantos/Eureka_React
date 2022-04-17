7f0347ef33aece470d6fea8406a275b3b60b381c
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-17 06:00:48 -0300

    criando as paginas

--- consulta-cep/src/App.js
+++ consulta-cep/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import  Pesquisa  from './Containers/Pesquisa'
+import  Carregando  from './Containers/Carregando'
+import  Erro  from './Containers/Erro'
+import  Resultados  from './Containers/Resultados'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return Resultados()
 }
 
-export default App;
+export default App

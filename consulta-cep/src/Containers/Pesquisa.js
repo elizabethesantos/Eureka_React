6903cf1a70f1613913b72e838e4542ca697dc128
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-19 23:58:31 -0300

    update app

--- consulta-cep/src/Containers/Pesquisa.js
+++ consulta-cep/src/Containers/Pesquisa.js
@@ -1,9 +1,48 @@
+import {useState} from 'react'
+import consultarCep from 'cep-promise'
+
+function numbersOnly(str){
+  return str.replace(/[^\d]/g, '')
+}
+
 function Pesquisa(props) {
   const goTo = props.goTo;
+  const setResultado = props.setResultado;
+  const [cepNumber, setCepNumber] = useState('');
+  function handleChange(evt) {
+    const value = evt.target.value
+    setCepNumber(numbersOnly(value))
+  }
+  function clear() {
+    setCepNumber('');
+  }
+  function handleSuccess(dadosCEP){
+    const resultados ={
+      "ESTADO": dadosCEP.state,
+      "CIDADE": dadosCEP.city,
+      "BAIRRO": dadosCEP.neighborhood,
+      "LOGRADOURO": dadosCEP.street
+    }
+    setResultado(resultados);
+    goTo("RESULTADOS");
+  }
+  function handleError(err){
+    const errorMessage = err.message;
+    setErrorMessage(errorMessage);
+    goTo("ERRO");
+  }
+  function handleSearch(){
+    goTo("CARREGANDO");
+    consultarCep(cepNumber)
+      .then(handleSuccess)
+      .catch(handleError)
+  }
   return <>
     <p>Qual CEP você deseja pesquisar?</p>
-    <input />
-    <button onClick={() =>goTo("RESULTADO") }>CONSULTAR</button>
+    <p>Estado atual : {cepNumber}</p>
+    <input value={numbersOnly(cepNumber)} onChange={handleChange} />
+    <button onClick={clear}>LIMPAR STATE</button>
+    <button onClick={handleSearch}>CONSULTAR</button>
   </>  
 }
 
--- consulta-cep/src/Containers/Resultados.js
+++ consulta-cep/src/Containers/Resultados.js
@@ -3,10 +3,10 @@ function Resultados(props) {
   const goTo = props.goTo;
   const keys = Object.keys(result);
   const elements = keys.map(key => (
-    <span key={key}><b>{keys}: </b>{result[key]}</span>
+    <span key={key}><b>{key}: </b>{result[key]}</span>
   ))
   return <>      
-    <p>Resultados para o CEP 16075-420</p>
+    <p>Resultados para o CEP {result.cep}</p>
     {elements}
     <button onClick={() => goTo("PESQUISA")}>NOVA CONSULTA</button>
   </>  
